20ecf21237219b14f61de301af44c77847f96e23
Alonso <alonso@avotz.com>
2018-08-29 12:31:21 -0600

    se cambio banners

--- wp-content/themes/grupodoit/page-home.php
+++ wp-content/themes/grupodoit/page-home.php
@@ -34,7 +34,7 @@ get_header();
                       </div>
 
                     </div>
-                    <div class="banner-slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/banner-do-2it.jpg')">
+                    <div class="banner-slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/banner1.jpg')">
                         <!-- <div class="banner-slide-title">
                           <?php if (get_locale() == 'es_ES') : ?>
                         <h2>Te ayudamos a construir tu futuro</h2>
@@ -45,7 +45,7 @@ get_header();
 
                         </div> -->
                     </div>
-                    <div class="banner-slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/do-it-edificio3.jpg')">
+                    <div class="banner-slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/banner2.jpg')">
                         <div class="banner-slide-title">
                         <?php if (get_locale() == 'es_ES') : ?>
                         <h2>Somos Grupo Do It</h2>
